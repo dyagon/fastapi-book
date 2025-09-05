825ba4824a8a7d8a243c6343f1e187434d58348d
yangdong <104878973+dyagon@users.noreply.github.com>
2025-09-05 19:50:30 +0800

    optimize ui

--- chatroom/main.py
+++ chatroom/main.py
@@ -1,9 +1,15 @@
+import pathlib
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 from .app.routers.user import router as user_router
 from .app.routers.room import router_chat
 
 app = FastAPI(title="Chat Room Application")
+
+static_dir = pathlib.Path(__file__).parent / "app" / "static"
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
 app.include_router(user_router)
